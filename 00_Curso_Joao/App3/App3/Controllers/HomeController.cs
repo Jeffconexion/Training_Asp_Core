2abd1b3223cd82c5131cf113e4caf1597de7c893
Jeff <jeff.debian2016@gmail.com>
2019-04-17 23:02:10 -0300

    Criando padrão MVC do 0

---
+++ 00_Curso_Joao/App3/App3/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult pag1()
+        {
+            return View();
+        }
+        public IActionResult pag2()
+        {
+            return View();
+        }
+        public IActionResult pag3()
+        {
+            return View();
+        }
+        public IActionResult pag4()
+        {
+            return View();
+        }
+    }
+}
