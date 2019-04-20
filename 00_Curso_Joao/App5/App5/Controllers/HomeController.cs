ee9d8c82bf577d9c7de8e1bee8958e18ef96af3e
Jeff <jeff.debian2016@gmail.com>
2019-04-20 13:04:42 -0300

    Passando dados do controle para view com MODEL ao invés de ViewBag ou ViewDate.

---
+++ 00_Curso_Joao/App5/App5/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Cliente novoCliente = new Cliente();
+            novoCliente.Nome = "teste";
+            novoCliente.apelido = "vamos nessa";
+            novoCliente.telefone = "333-333";
+
+
+            return View(novoCliente);
+        }
+    }
+}
---
+++ 00_Curso_Joao/App5/App5/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App5.Models
+{
+    public class Cliente
+    {
+        public string Nome { set; get; }
+        public string apelido { set; get; }
+        public string telefone { get; set; }
+    }
+}
