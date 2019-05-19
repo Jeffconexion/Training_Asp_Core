cacfa0cb0f04a864be1f1648f05489223a15eb9e
Jefferson Santos <jeff.debian2016@gmail.com>
2019-05-19 11:21:34 -0300

    Acomplando vários models e passando por um único local de uma View.

---
+++ 00_Curso_Joao/App9/App9/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App9.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Humano homem = new Humano()
+            {
+                Nome = "Naturo",
+                Apelido = "Yuzumaki"
+            };
+
+            Automovel carro = new Automovel()
+            {
+                Marca = "Corolla",
+                Cor = "azul"
+            };
+
+            box conteiner = new box()
+            {
+                humano = homem,
+                automovel = carro
+            };
+
+            return View(conteiner);
+        }
+    }
+}
---
+++ 00_Curso_Joao/App9/App9/Models/Dados.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App9.Models
+{
+    public class Humano
+    {
+        public string Nome { set; get; }
+        public string Apelido { set; get; }
+    }
+
+    public class Automovel
+    {
+        public string Marca { set; get; }
+        public string Cor { set; get; }
+    }
+
+    public class box
+    {
+        public Humano humano { set; get; }
+        public Automovel automovel { set; get; }
+    }
+}
