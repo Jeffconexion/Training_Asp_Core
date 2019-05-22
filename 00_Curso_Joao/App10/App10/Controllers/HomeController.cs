f3e257907a21b55aada16d98d5c5c41d38044d96
Jefferson Santos <jeff.debian2016@gmail.com>
2019-05-22 16:49:49 -0300

    Acoplando classes e utilizando partial.

---
+++ 00_Curso_Joao/App10/App10/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using App10.Model;
+
+namespace App10.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            Humano pessoa = new Humano()
+            {
+                Nome = "Yuri"
+            };
+
+            Automóvel carro = new Automóvel()
+            {
+                TipoCarro = "Corola"
+            };
+
+            Dados registrarDados = new Dados()
+            {
+                humano = pessoa,
+                automovel = carro
+            };
+
+            return View(registrarDados);
+        }
+    }
+}
---
+++ 00_Curso_Joao/App10/App10/Model/Dados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App10.Model
+{
+    public class Humano
+    {
+        public string Nome { set; get; }
+    }
+
+    public class Automóvel
+    {
+        public string TipoCarro { set; get; }
+    }
+
+    public class Dados
+    {
+        public Humano humano { set; get; }
+        public Automóvel automovel { set; get; }
+    }
+}
