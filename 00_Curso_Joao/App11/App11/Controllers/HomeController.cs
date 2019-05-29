7368056a7efdf07c08ffe85fb17e5527ada48715
Jefferson Santos <jeff.debian2016@gmail.com>
2019-05-29 00:28:14 -0300

    Modificando o Route Maps.

---
+++ 00_Curso_Joao/App11/App11/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App11.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string PaginaTeste(int valor)
+        {
+            //Não consigo passar o valor
+            return  valor.ToString();
+        }
+    }
+}
