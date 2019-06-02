42f56b0316ccb08b519d53907adc29b6a85a6967
Jefferson Santos <jeff.debian2016@gmail.com>
2019-06-02 19:47:50 -0300

    Utilizando Route como atributo.(home/acao/{nome?}/{sobrenome?}

---
+++ 00_Curso_Joao/App12/App12/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App12.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Home/Index")]
+        [Route("Home/aaa")]
+        public string Index()
+        {
+            return "Estou no INDEX";
+        }
+
+        [Route("Home/ppp/{id}")]
+        public string acao1(int id)
+        {
+            return "Estou na acao 1 = "+id;
+        }
+
+        [Route("home/post")]
+        public string acao2()
+        {
+            return "estou na acao 2";
+        }
+        [Route("home/valor/{nome?}/{sobrenome?}")]
+        public string valor(string nome, string sobrenome)
+        {
+            return " Valor:"+nome+" "+ sobrenome;
+        }
+    }
+}
