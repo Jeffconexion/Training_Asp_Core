b2f1e4317c87b58baa0ebca5222ba0db445746a2
Jefferson Santos <jeff.debian2016@gmail.com>
2019-08-09 09:39:47 -0300

    Criando uma classe cliente e model tipificado, sem utilizar lista, o básico entre model e view.

---
+++ 00_Curso_Joao/TrabalhandoPersistence/App20/App20/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using App20.Models;
+
+namespace App20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Clientes cliente_novo = new Clientes();
+            cliente_novo.nome = "Yuri";
+            cliente_novo.apelido = "Silva";
+
+            return View(cliente_novo);
+        }
+    }
+}
---
+++ 00_Curso_Joao/TrabalhandoPersistence/App20/App20/Models/Clientes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App20.Models
+{
+    public class Clientes
+    {
+        public string nome { get; set; }
+        public string apelido { get; set; }
+    }
+}
