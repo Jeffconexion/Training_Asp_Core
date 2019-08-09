7633349d9a15b59e0e5a62565de11a86a5c6dfbb
Jefferson Santos <jeff.debian2016@gmail.com>
2019-08-09 14:42:54 -0300

    Passando uma coleção de alunos, ao inves de utilizar INUmerables usa List.

---
+++ 00_Curso_Joao/TrabalhandoPersistence/App21/App21/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using App21.Model;
+
+namespace App21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Aluno> alunos = new List<Aluno>()
+            {
+                new Aluno(){  nome="Tiago"},
+                new Aluno(){  nome="Marcos"},
+                new Aluno(){  nome="Pato"}
+
+
+            };
+
+
+
+
+            return View(alunos);
+        }
+    }
+}
---
+++ 00_Curso_Joao/TrabalhandoPersistence/App21/App21/Model/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App21.Model
+{
+    public class Aluno
+    {
+        public string nome { get; set; }
+    }
+}
