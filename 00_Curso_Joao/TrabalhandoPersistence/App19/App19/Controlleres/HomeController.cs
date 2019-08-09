05b7c05588297cebb80ab203292fa8c51bddc861
Jefferson Santos <jeff.debian2016@gmail.com>
2019-08-09 07:59:44 -0300

    Criando o model cliente e exibindo através de um template List.

---
+++ 00_Curso_Joao/TrabalhandoPersistence/App19/App19/Controlleres/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using App19.Models;
+
+
+namespace App19.Controlleres
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Cliente> lista_clientes = new List<Cliente>();
+            lista_clientes.Add(new Cliente() { nome = "Marcos", apelido = "Souza" });
+            lista_clientes.Add(new Cliente() { nome = "Ana", apelido = "Oliveira" });
+            lista_clientes.Add(new Cliente() { nome = "Carlos", apelido = "Morgan" });
+
+             
+            return View(lista_clientes);
+        }
+    }
+}
