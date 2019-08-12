a0c8ec36236c2ab3ec7e8ad2accc841bdbd464e6
Jefferson Santos <jeff.debian2016@gmail.com>
2019-08-12 00:44:01 -0300

    Criando uma lista de objetos, colocando a opção de detalhes, selecionando o cliente a ser visualizado.

---
+++ 00_Curso_Joao/TrabalhandoPersistence/App22/App22/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using App22.Models;
+
+namespace App22.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Cliente> lista_cliente;
+
+        public HomeController()
+        {
+            lista_cliente = new List<Cliente>()
+            {
+                new Cliente(){ Matricula = 0, Nome = "Joao", Cargo = "Desenvolvedor" },
+                new Cliente(){ Matricula = 1, Nome = "Ana", Cargo = "Analista" },
+                new Cliente(){ Matricula = 2, Nome = "Carlos", Cargo = "Supervisor" }
+
+
+            };
+        }
+        public IActionResult Index()
+        {
+            return View(lista_cliente);
+        }
+
+        public ViewResult Detalhes(int matricula)
+        {
+            return View(lista_cliente[matricula]);
+        }
+    }
+}
